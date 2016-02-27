cb1d79916969f53971256f6e17b03e9a3fb150dc
Wei <weizhang.zhang1988@gmail.com>
2016-02-27 13:42:34 -0500

    add tab bar icon

--- WZTextField/CustomTabBarViewController.m
+++ WZTextField/CustomTabBarViewController.m
@@ -33,25 +33,29 @@
 #pragma mark - set up
 - (void)setupTabViewControllers {
     NSMutableArray *tabViewControllers = [[NSMutableArray alloc] init];
+    UIImage *unselectedImage = [UIImage imageNamed:@"Star-grey.png"];
+    unselectedImage = [unselectedImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    UIImage *selectedImage = [UIImage imageNamed:@"Star-red.png"];
+    selectedImage = [selectedImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     // 1. first tab
     FirstTabViewController *firstVC = [[FirstTabViewController alloc] init];
     UINavigationController *firstNav = [[UINavigationController alloc] initWithRootViewController:firstVC];
-    firstVC.title = @"HomePage";
+    firstVC.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"HomePage" image:unselectedImage selectedImage:selectedImage];
     [tabViewControllers addObject:firstNav];
     // 2. second tab
     SecondTabViewController *secondVC = [[SecondTabViewController alloc] init];
     UINavigationController *secondNav = [[UINavigationController alloc] initWithRootViewController:secondVC];
-    secondVC.title = @"Promotion";
+    secondVC.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Promotion" image:unselectedImage selectedImage:selectedImage];
     [tabViewControllers addObject:secondNav];
     // 3 third tab
     ThirdTabViewController *thirdVC = [[ThirdTabViewController alloc] init];
     UINavigationController *thirdNav = [[UINavigationController alloc] initWithRootViewController:thirdVC];
-    thirdVC.title = @"Chat";
+    thirdVC.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Chat" image:unselectedImage selectedImage:selectedImage];
     [tabViewControllers addObject:thirdNav];
     // 4. fourth tab
     FourthTabViewController *fourthVC = [[FourthTabViewController alloc] init];
     UINavigationController *fourthNav = [[UINavigationController alloc] initWithRootViewController:fourthVC];
-    fourthVC.title = @"MyProfile";
+    fourthVC.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"MyProfile" image:unselectedImage selectedImage:selectedImage];
     [tabViewControllers addObject:fourthNav];
     self.viewControllers = tabViewControllers;
     
